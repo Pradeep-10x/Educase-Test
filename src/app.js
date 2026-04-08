fe0034b787c7522e14eefe7291e558d8da2ddd9e
Pradeep <pradeep2ndsingh@gmail.com>
2026-04-09 01:45:34 +0530

    live api

--- src/app.js
+++ src/app.js
@@ -2,14 +2,23 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
+import { createRequire } from 'module';
+import swaggerUi from 'swagger-ui-express';
+
 import initializeDatabase from './config/initDb.js';
 import schoolRoutes from './routes/schoolRoutes.js';
+
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
+
+// Swagger Live Api
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use('/api', schoolRoutes);
